f12c5f85b973c4a4e21b39ff959a43fa58e40d36
willycole12345 <cole.william043@gmail.com>
2022-01-06 00:31:28 +0100

    new update

--- pages/signin.js
+++ pages/signin.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link';
-//import Router from 'next/router'
-import React from 'react'
+import styles from '../styles/Home.module.css'
+import React from 'react';
+import Router from 'next/router';
 import {FaFacebookF, FaLinkedinIn, FaGoogle, FaRegEnvelope} from 'react-icons/fa';
 import { MdLockOutline }  from 'react-icons/md';
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
-export default function signup() {
- 
-  
+
+
+export default function Home() {
+   
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
     <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -23,27 +24,29 @@ export default function signup() {
             <div className="py-10">
                 <h2 className="text-3xl font-bold text-green-500 mb-2">Sign In to Account</h2>
                         <div className="border-2 w-10 border-white inline-block mb-2"></div>
+                        <div className="flex justify-center my-2">
+                          <a href="" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                            <FaFacebookF className="text-sm"/>
+                          </a>
+                          <a href="" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                            <FaLinkedinIn className="text-sm"/>
+                          </a>
+                          <a href="" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+                            <FaGoogle className="text-sm"/>
+                          </a>
+
+                        </div>
                         <p className="text-gray-400 my-3">or use your Email Account</p>
-                        <form > 
+                        <form> 
                         <div className="flex flex-col items-center">
-                        <div  className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                        <div htmlFor="email"  className="bg-gray-100 w-64 p-2 flex items-center mb-3">
                           <FaRegEnvelope  className="text-gray-400 m-2"/>
-                          <input text="name" name="name" id="name" 
-                            
-                              placeholder="FullName"  className="bg-gray-100 outline-none text-sm"/> 
+                          <input text="email" name="email" id="email"
+                              placeholder="Email"  className="bg-gray-100 outline-none text-sm"/> 
                         </div>
-                        <div  className="bg-gray-100 w-64 p-2 flex items-center mb-3">
-                          <FaRegEnvelope  className="text-gray-400 m-2"/>
-                          <input text="email" name="email" id="email" placeholder="Email"  className="bg-gray-100 outline-none text-sm"/> 
-                        </div>
-                        <div  className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                        <div htmlFor="password" className="bg-gray-100 w-64 p-2 flex items-center mb-3">
                           <MdLockOutline className="text-gray-400 m-2"/>
-                          <input type="password" name="password" id="password" placeholder="password" className="bg-gray-100 outline-none text-sm"/> 
-                        </div>
-                        <div  className="bg-gray-100 w-64 p-2 flex items-center mb-3">
-                          <MdLockOutline className="text-gray-400 m-2"/>
-                          <input type="password" name="repassword" id="repassword" placeholder="repassword" 
-                            className="bg-gray-100 outline-none text-sm"/> 
+                          <input type="password" name="password" id="password" placeholder="password"  className="bg-gray-100 outline-none text-sm"/> 
                         </div>
                         <div className="flex justify-between w-64 mb-5">
                           <label className="flex item-center text-xs">
@@ -65,10 +68,10 @@ export default function signup() {
         <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl"> 
         <h2 className="test-3xl font-bold mb-2">Hi There </h2>
         <div className="border-2 w-10 border-white inline-block mb-2"></div>
-        <p className='mb-2'> Already have an Account </p>
-        <Link href="/signin">
+        <p className='mb-2'> Register on our join us </p>
+        <Link href="/">
         <a  className='border-2 border-white rounded-full px-12 py-2 inline-block font-semibo 
-         ld hover:bg-white hover:text-green-500'> Sign in</a>
+         ld hover:bg-white hover:text-green-500'> Sign up</a>
          </Link>
         </div>
       </div>
